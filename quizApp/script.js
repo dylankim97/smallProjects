c90d04a866ecd34140a6adf0fdb5c64f2ca1456c
dylankim97 <kimdl2373@gmail.com>
2021-07-20 08:22:43 +0900

    Start quizApp project, layout basic structure

---
+++ quizApp/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Dylan?',
+        a: '10',
+        b: '15',
+        c: '23',
+        d: '30',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    }, {
+        question: "Who is the President of US?",
+        a: 'Kanye West',
+        b: 'Joe Biden',
+        c: 'Donald Trump',
+        d: 'George Bush',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
