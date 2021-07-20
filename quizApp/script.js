03d49019b2e5b22b57a935e408043483f0006a0a
dylankim97 <kimdl2373@gmail.com>
2021-07-20 09:17:53 +0900

    Add questions, answers, and styles

--- quizApp/script.js
+++ quizApp/script.js
@@ -35,4 +35,54 @@ const quizData = [
         d: 'none of the above',
         correct: 'd'
     }
-]
+];
+
+const questionE1 = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let answer = undefined;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData
+    [currentQuiz];
+    questionE1.innerHTML = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz++;
+
+    getSelected();
+    /*
+    if(currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        //TODO: show results
+        alert('You finished!')
+    }
+    */
+
+
+})
+
+function getSelected() {
+    const answerE1s = document.querySelectorAll(".answer");
+
+
+    answerE1s.forEach((answerE1) => {
+        if(answerE1.checked) {
+            answer = answerE1.id;
+        }
+    });
+}
